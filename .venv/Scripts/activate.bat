9e098576f5cf23b76737a2371da176665983ae0e
MysticDaven <92825671+MysticDaven@users.noreply.github.com>
2024-11-12 09:33:56 -0600

    Mini cambios
    Se hace el push, para poder trabajar en la máquina del trabajo.

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\AIDD010822\Documents\Daven\ITM\Titulación\defense-against-ransomware\.venv
+set VIRTUAL_ENV=C:\Users\daven\Documents\ITMorelia\Residencias\DPAR\defense-against-ransomware\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
